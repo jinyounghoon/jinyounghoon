40ee1e2a365f8f4aefbc3edd7d00b8022347393e
jihyounghoon <jin304802@naver.com>
2019-10-09 16:40:50 +0900

    Update move_character_with_mouse.py
    캐릭터 출력 추가 ( 3회)

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -6,14 +6,19 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
    global running
    global x, y
+   global z, c
    events = get_events()
    for event in events:
        if event.type == SDL_QUIT:
            running = False
        elif event.type == SDL_MOUSEMOTION:
            x, y = event.x, KPU_HEIGHT - 1 - event.y
+       elif event.type == SDL_MOUSEBUTTONDOWN:
+           if event.key == SDL_BUTTON_LEFT:
+               z, c = event.x, KPU_HEIGHT - 1 - event.y
        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-           running = False
+            running = False
+
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -23,6 +28,7 @@ mouse = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+z, c = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
@@ -31,7 +37,7 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.clip_draw(0, 0, 100, 100, x, y)
-    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, z, c)
     update_canvas()
     frame = (frame + 1) % 8
 
